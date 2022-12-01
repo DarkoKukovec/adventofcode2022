7363c845cd92d4543ccf086c5a8ea03f95c1e995
Darko Kukovec <darko@infinum.com>
2022-12-01 17:28:15 +0100

    Styling

--- www/index.js
+++ www/index.js
@@ -2,11 +2,34 @@ import * as wasm from "adventofcode2022";
 
 window.log = console.log.bind(console);
 
+document.querySelectorAll('.input').forEach((el) => {
+  const lines = el.value.split('\n').length;
+  el.setAttribute('rows', lines);
+
+  el.addEventListener('input', () => {
+    const lines = el.value.split('\n').length;
+    el.setAttribute('rows', lines);
+  });
+});
+
+document.querySelectorAll('h3').forEach((el) => {
+  const id = el.parentElement.getAttribute('data-id');
+  el.innerHTML = `Day ${id}`;
+  el.addEventListener('click', () => {
+    const opened = document.querySelector('.opened')
+    if (opened) opened.classList.remove('opened');
+    el.parentElement.classList.add('opened');
+  });
+});
+
+document.querySelector('h3').parentElement.classList.add('opened');
+
 document.querySelectorAll('.exec').forEach((el) => {
   el.addEventListener('click', () => {
-    const id = el.getAttribute('data-id');
-    const input = document.querySelector(`#input-${id}`).value;
-    const output = document.querySelector(`#output-${id}`);
+    const target = el.parentElement.parentElement;
+    const id = target.getAttribute('data-id');
+    const input = target.querySelector('.input').value;
+    const output = target.querySelector('.output');
     el.setAttribute('disabled', 'disabled');
     output.innerHTML = wasm[`exec_${id}`](input) || 'Nothing to execute';
     el.removeAttribute('disabled');
